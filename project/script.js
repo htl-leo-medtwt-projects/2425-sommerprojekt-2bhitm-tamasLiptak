fa57b8c2ce97ef7477883660c91ada7a481d1875
Tamas Liptak <t.liptak@students.htl-leonding.ac.at>
2025-04-24 20:40:05 +0200

    first gsap usage

--- project/script.js
+++ project/script.js
@@ -1,3 +1,19 @@
 function toggleMenu() {
-    document.getElementById("navLinks").classList.toggle("active");
-  }
+  document.getElementById("navLinks").classList.toggle("active");
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".lockerweaponimg", {
+  scrollTrigger: {
+    trigger: ".lockerweaponimg",
+    toggleActions: "restart pause reverse pause",
+    start: "top 80%",
+    scrub: 1,
+    end: "top 20%",
+  },
+  x: 700,
+  rotation: 45,
+  scale: 0.5,
+  duration: 2
+});
